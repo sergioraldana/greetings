f76a4970c2b9ad04ee965a9a6039193f1a390fd5
Sergio Renato Aldana Alvarez <sergio.aldana@galileo.edu>
2022-06-13 21:02:06 -0600

    Save and display greeting messages

--- index.php
+++ index.php
@@ -45,12 +45,31 @@ if (isloggedin()) {
 
 $messageform->display();
 
+$messages = $DB->get_records('local_greetings_messages');
+
+echo $OUTPUT->box_start('card-columns');
+foreach ($messages as $m) {
+    echo html_writer::start_tag('div', array('class' => 'card'));
+    echo html_writer::start_tag('div', array('class' => 'card-body'));
+    echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::start_tag('p', array('class' => 'card-text'));
+    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
+    echo html_writer::end_tag('p');
+    echo html_writer::end_tag('div');
+    echo html_writer::end_tag('div');
+}
+echo $OUTPUT->box_end();
+
+
 if ($data = $messageform->get_data()) {
     $message = required_param('message', PARAM_TEXT);
 
-    echo $OUTPUT->heading($message, 4);
+    if (!empty($message)) {
+        $record = new stdClass();
+        $record->message = $message;
+        $record->timecreated = time();
+        $DB->insert_record('local_greetings_messages', $record);
+    }
 }
 
 echo $OUTPUT->footer();
-
-
